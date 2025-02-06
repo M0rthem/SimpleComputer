53748590f275c3af623b7b30df4cb508f2c79b24
Nesterov Ilya <ilianesterov95@gmail.com>
2025-02-06 21:41:55 +0700

    add sc_accumulatorGet function

---
+++ mySipleComputer/sc_accumulatorGet.c
@@ -0,0 +1,10 @@
+#include "../include/register.h"
+#include <stdio.h>
+int sc_accumulatorGet(int* value)
+{
+    if (value == NULL) {
+        return -1;
+    }
+    *value = accumulator.var;
+    return 0;
+}
