bd880ada823e2ee8a9112e18ad6b1bb63db1570e
Nesterov Ilya <ilianesterov95@gmail.com>
2025-02-06 13:23:50 +0700

    add registers value

---
+++ include/register.h
@@ -0,0 +1,8 @@
+#ifndef REGISTER_H
+#define REGISTER_H
+
+extern int accumulator;
+extern int commandCounter;
+extern int flagsRegister;
+
+#endif
--- mySipleComputer/sc_variables.c
+++ mySipleComputer/sc_variables.c
@@ -1,2 +1,8 @@
 #include "../include/mem.h"
+#include "../include/register.h"
+
 int memory[memorySize];
+
+int accumulator;
+int commandCounter;
+int flagsRegister;
