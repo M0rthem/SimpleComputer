c92338ef0a8c9d7108b5bf8da78dcec368b9bfbf
Nesterov Ilya <ilianesterov95@gmail.com>
2025-02-06 15:42:00 +0700

    add sc_regSet function

--- include/register.h
+++ include/register.h
@@ -3,6 +3,12 @@
 
 extern int accumulator;
 extern int commandCounter;
+
 extern int flagsRegister;
+#define REGISTER_OVERFLOW 1
+#define REGISTER_DIVIZION_ZERO 2
+#define REGISTER_MEMORY_OUT 4
+#define REGISTER_IGNORE_TACT 8
+#define REGISTER_INCORRECT_COMMAND 16
 
 #endif
