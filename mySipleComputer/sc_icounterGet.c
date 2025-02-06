3bf885e24c85b5cc52de3e80e9a71190922d434f
Nesterov Ilya <ilianesterov95@gmail.com>
2025-02-06 21:53:04 +0700

    add function sc_icounterGet

---
+++ mySipleComputer/sc_icounterGet.c
@@ -0,0 +1,11 @@
+#include "../include/register.h"
+#include <stdio.h>
+
+int sc_icounterGet(int* value)
+{
+    if (value == NULL) {
+        return -1;
+    }
+    *value = commandCounter.var;
+    return 0;
+}
