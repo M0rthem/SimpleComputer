d453b4224d7deed49b896ac05366fc006cc0f80c
Nesterov Ilya <ilianesterov95@gmail.com>
2025-02-06 11:20:12 +0700

    add sc_memoryGet.c function

---
+++ mySipleComputer/sc_memoryGet.c
@@ -0,0 +1,10 @@
+#include "../include/mem.h"
+
+int sc_memoryGet(int address, int* value)
+{
+    if (address < 0 || address >= memorySize) {
+        return -1;
+    }
+    *value = memory[address];
+    return 0;
+}
