afed774d7f58645f9e24257d181229e3daadaee3
Nesterov Ilya <ilianesterov95@gmail.com>
2025-02-07 16:06:21 +0700

    add printDecodedCommand

---
+++ console/printDecodedCommand.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void printDecodedCommand(int value)
+{
+    printf("dec: %0*u | oct: %0*o | hex: %0*X | bin: ",
+           5,
+           value,
+           5,
+           value,
+           4,
+           value);
+    for (int i = 15 - 1; i >= 0; i--) {
+        putchar((value >> i) & 1 ? '1' : '0');
+    }
+}
