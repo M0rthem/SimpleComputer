81ed8586dee2e686b6039bc0c6206df63fcbb7e2
Nesterov Ilya <ilianesterov95@gmail.com>
2025-02-11 12:33:09 +0700

    add test_terminal.sh

---
+++ test_terminal.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo -e "\E[H\E[J"
+echo -e "\E[40m"
+echo -e "\E[31m"
+echo -e "\E[5;10HIlya"
+
+echo -e "\E[47m"
+echo -e "\E[32m"
+
+
+echo -e "\E[6;8HIS-342"
+
+echo -e "\E[0m"
+echo -e "\E[10;1H"
