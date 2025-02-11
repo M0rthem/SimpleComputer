e80905b1edaa5e5b1bcf159025680d338922cb82
Nesterov Ilya <ilianesterov95@gmail.com>
2025-02-11 23:03:47 +0700

    update printCounter

--- console/src/printCounters.c
+++ console/src/printCounters.c
@@ -1,9 +1,23 @@
 #include "mySimpleComputer.h"
+#include "myTerm.h"
 #include <stdio.h>
 
 void printCounters(void)
 {
+    mt_gotoXY(60, 5);
+
     int value;
+    int sign;
+    int command;
+    int opperand;
     sc_icounterGet(&value);
-    printf("%d", value);
+    sc_commandDecode(value, &sign, &command, &opperand);
+
+    printf("T: 00     IC: ");
+    if (sign == 0) {
+        putchar('+');
+    } else {
+        putchar('-');
+    }
+    printf("%0*X%0*x", 2, command, 2, opperand);
 }
