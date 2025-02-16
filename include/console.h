c6dbde79c3b44110b65ac2b7687c11ce9ebab903
Nesterov Ilya <ilianesterov95@gmail.com>
2025-02-16 19:45:50 +0700

    update console.h

--- include/console.h
+++ include/console.h
@@ -1,8 +1,9 @@
 #ifndef CONSOLE_H
 #define CONSOLE_H
 
+#include "myTerm.h"
 void printAccumulator(void);
-void printCell(int address);
+void printCell(int address, enum colors fg, enum colors bg);
 void printCounters(void);
 void printDecodedCommand(int value);
 void printFlags(void);
