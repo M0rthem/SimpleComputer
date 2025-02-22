a296a2a5e34466825abec6643f96c8d5a1362775
Nesterov Ilya <ilianesterov95@gmail.com>
2025-02-23 00:07:59 +0700

    add bc_printbigchar func

--- include/myBigChars.h
+++ include/myBigChars.h
@@ -4,6 +4,7 @@
 int bc_strlen(char* str);
 int bc_printA(char* str);
 int bc_setbigcharpos(int* big, int x, int y, int value);
-int bc_getbigcharpos(int* big, int x, int y, int* value)
+int bc_getbigcharpos(int* big, int x, int y, int* value);
+int bc_printbigchar(int big[2], int x, int y, enum colors fg, enum colors bg);
 
 #endif
