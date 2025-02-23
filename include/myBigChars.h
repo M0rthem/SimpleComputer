f60b53ca4ad7590710620beb28dcd06376391df3
Nesterov Ilya <ilianesterov95@gmail.com>
2025-02-24 01:10:42 +0700

    update myBigChars.h

--- include/myBigChars.h
+++ include/myBigChars.h
@@ -11,4 +11,16 @@ int bc_printbigchar(int big[2], int x, int y, enum colors fg, enum colors bg);
 int bc_bigcharwrite(int fd, int* big, int count);
 int bc_bigcahrread(int fd, int* big, int need_count, int* count);
 
+int bc_box(
+        int x1,
+        int y1,
+        int x2,
+        int y2,
+        enum colors box_fg,
+        enum colors box_bg,
+        char* header,
+        enum colors header_fg,
+        enum colors header_bg);
+
+extern int bigchar[18][2];
 #endif
