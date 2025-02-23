49e706d669caad9ba8e352e6db3e82ff8965c8d2
Nesterov Ilya <ilianesterov95@gmail.com>
2025-02-23 16:04:13 +0700

    update bc_bigcharread func

--- myBigChars/src/bc_bigcharread.c
+++ myBigChars/src/bc_bigcharread.c
@@ -2,6 +2,8 @@
 
 int bc_bigcahrread(int fd, int* big, int need_count, int* count)
 {
+    if (!big || !count)
+        return -1;
     int flag;
     flag = read(fd, big, need_count * sizeof(int) * 2);
     if ((flag / (sizeof(int) * 2)) == need_count) {
