0386e93b44f4e35b900cff3eaba142d742114de9
Nesterov Ilya <ilianesterov95@gmail.com>
2025-03-09 23:15:22 +0700

    change int on usnsigned int in memory

--- include/mytype.h
+++ include/mytype.h
@@ -2,7 +2,7 @@
 #define MYTYPE_H
 
 typedef struct {
-    int var : 15;
+    unsigned int var : 15;
 } int_15bit;
 
 #endif
--- mySimpleComputer/src/sc_memorySet.c
+++ mySimpleComputer/src/sc_memorySet.c
@@ -5,7 +5,7 @@ int sc_memorySet(int address, int value)
     if (address < 0 || address >= memorySize) {
         return -1;
     }
-    if (value < -16384 || value > 16383) {
+    if (value > 0 && value < 0x7FF) {
         return -1;
     }
     memory[address].var = value;
